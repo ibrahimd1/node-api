9db14543fba8e528bf73c9e34ba03b5b0257b61e
Ibrahim Darakcilar <idarakcilar@gmail.com>
2019-09-22 23:17:16 +0300

    modify

--- routes/movie.js
+++ routes/movie.js
@@ -4,6 +4,68 @@ var router = express.Router();
 //models
 const Movie = require("../models/Movie.js");
 
+router.get("/", (req, res) => {
+  const promise = Movie.find({});
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/top10", (req, res) => {
+  const promise = Movie.find({})
+    .limit(10)
+    .sort({ imdb_score: 1 });
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/:movie_id", (req, res) => {
+  const promise = Movie.find({ _id: req.params.movie_id });
+  promise
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.put("/:movie_id", (req, res, next) => {
+  const promise = Movie.findByIdAndUpdate(req.params.movie_id, req.body, {
+    new: true
+  });
+  promise
+    .then(data => {
+      if (!data) {
+        next({ code: -1, message: "The movie was not found!" });
+      }
+      res.json({ statusCode: 1, message: "Success!" });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.delete("/:movie_id", (req, res) => {
+  const promise = Movie.findOneAndDelete(req.params.movie_id);
+  promise
+    .then(data => {
+      res.json({ statusCode: 1, message: data.id + "id deleted" });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
 router.post("/", function(req, res, next) {
   const { title, category, country, year, imdb_score } = req.body;
 
@@ -15,15 +77,24 @@ router.post("/", function(req, res, next) {
     imdb_score: imdb_score
   });
 
-  // movie.save((err, data) => {
-  //   if (err) res.json(err);
-  //   res.json(data);
-  // });
-
   const promise = movie.save();
   promise
     .then(data => {
       res.json({ status: 1 });
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/between/:start_date/:end_date", (req, res) => {
+  const { start_date, end_date } = req.params;
+  const promise = Movie.find({
+    year: { $gte: parseInt(start_date), $lte: parseInt(end_date) }
+  });
+  promise
+    .then(data => {
+      res.json(data);
     })
     .catch(err => {
       res.json(err);
