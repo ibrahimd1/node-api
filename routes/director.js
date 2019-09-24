ad3a58929b6ca4e35e9edb64160f4f5bb9eaefa5
Ibrahim Darakcilar <idarakcilar@gmail.com>
2019-09-24 21:56:06 +0300

    api/director/director_id

--- routes/director.js
+++ routes/director.js
@@ -1,3 +1,4 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
@@ -52,20 +53,51 @@ router.get("/", (req, res) => {
     });
 });
 
-router.post("/", (req, res) => {
-  const director = new Director(req.body);
-  const promise = director.save();
-  promise
-    .then(data => {
-      res.json({ statusCode: 1, message: "Success!" });
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
 router.get("/:director_id", (req, res) => {
-  const promise = Director.findById(req.params.director_id);
+  const promise = Director.aggregate([
+    {
+      $match: {
+        _id: mongoose.Types.ObjectId(req.params.director_id)
+      }
+    },
+    {
+      $lookup: {
+        from: "movies",
+        localField: "_id",
+        foreignField: "director_Id",
+        as: "movies"
+      }
+    },
+    {
+      $unwind: {
+        path: "$movies",
+        preserveNullAndEmptyArrays: true
+      }
+    },
+    {
+      $group: {
+        _id: {
+          _id: "$_id",
+          name: "$name",
+          surname: "$surname",
+          bio: "$bio"
+        },
+        movies: {
+          $push: "$movies"
+        }
+      }
+    },
+    {
+      $project: {
+        _id: "$_id._id",
+        name: "$_id.name",
+        surname: "$_id.surname",
+        bio: "$_id.bio",
+        movies: "$movies"
+      }
+    }
+  ]);
+
   promise
     .then(data => {
       res.json(data);
@@ -74,31 +106,5 @@ router.get("/:director_id", (req, res) => {
       res.json(err);
     });
 });
-
-router.put("/:director_id", (req, res) => {
-  const promise = Director.findByIdAndUpdate(req.params.director_id, req.body, {
-    new: true
-  });
-  promise
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-router.delete("/:director_id", (req, res) => {
-  const promise = Director.findByIdAndRemove(req.params.director_id);
-  promise
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-router.get("/best10movie");
 
 module.exports = router;
